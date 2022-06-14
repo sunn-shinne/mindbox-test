16a96053e7896f3780aeab9017e0d0827a1e66a3
Ekaterina <brekhova.katya@gmail.com>
2022-06-14 10:13:20 +0700

    Refactoring

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import { I18nextProvider } from 'react-i18next';
 import i18n from './i18n.js';
 import store from './slices/index.js';
 import App from './App.js';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -18,8 +17,3 @@ root.render(
     </I18nextProvider>
   </React.StrictMode>,
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
